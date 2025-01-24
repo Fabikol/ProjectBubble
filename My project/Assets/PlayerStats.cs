0938975f98992a8957c5231daf54b5fb53ff8a29
Maximilian BEcker <maximilian.becker@stud.h-da.de>
2025-01-25 00:10:58 +0100

    [+Playerstats script]
    test

---
+++ My project/Assets/PlayerStats.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //This script will be used for stats like maxSpeed etc...
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
