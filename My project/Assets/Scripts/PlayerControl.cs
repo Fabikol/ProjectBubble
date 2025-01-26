6f279022a729181a92e2f753c828d0ae1a697522
Fabian Reschke <fabianreschke123@gmail.com>
2025-01-26 09:09:23 +0100

    Respawn in PlayerControl added

--- My project/Assets/Scripts/PlayerControl.cs
+++ My project/Assets/Scripts/PlayerControl.cs
@@ -8,11 +8,17 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float acceleration = 10f;
     [SerializeField] public float maxSpeed = 100f;
     [SerializeField] private float jumpForce = 50;
+    [SerializeField] private float dashForce = 20;
+    
+    private bool isOnGround = false;
 
     public GameObject Respawnpoint;
 
+    
+    public Transform cameraTransform;
 
     private Vector2 moveInputValue;
+    private bool attackPressed=false;
 
     private void OnMove(InputValue value)
     {
@@ -30,20 +36,65 @@ public class PlayerControl : MonoBehaviour
 
     private void MoveLogic()
     {
-
-        Vector2 result = moveInputValue * acceleration * Time.fixedDeltaTime;
-        if (result != Vector2.zero)
+        
+        Vector3 result = new Vector3(moveInputValue.x, 0f, moveInputValue.y) * acceleration * Time.fixedDeltaTime;
+        
+        
+        
+        if (result != Vector3.zero)
         {
-            float speed = new Vector2(rb.linearVelocity.x, rb.linearVelocity.z).magnitude + result.magnitude;
+            float targetAngle = Mathf.Atan2(result.x, result.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            
+            float speed = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z).magnitude + result.magnitude;
             float fallSpeed = rb.linearVelocity.y;
-            rb.linearVelocity = new Vector3(result.x, 0, result.y).normalized * speed + new Vector3(0, fallSpeed, 0);
+            Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
+            rb.linearVelocity = moveDir * speed + new Vector3(0, fallSpeed, 0);
+            
+            if (attackPressed)
+            {
+                attackPressed = false;
+                Vector3 dashDirection = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+                rb.linearVelocity = Vector3.zero;
+                rb.AddForce(dashDirection.normalized * dashForce, ForceMode.VelocityChange);
+            }
         }
+        else
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x * 0.9f, rb.linearVelocity.y, rb.linearVelocity.z * 0.9f);
+        }
+    }
+    
+
+    private void OnAttack(InputValue value)
+    {
+        //Debug.Log("Attack");
+        attackPressed = true;
     }
 
     private void FixedUpdate()
     {
         MoveLogic();
 
+    }
+    
+    
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("world"))
+        {
+            //Debug.Log("Boden");
+            isOnGround = true;
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.layer==LayerMask.NameToLayer("world"))
+        {
+            //Debug.Log("Luft");
+            isOnGround = false;
+        }
     }
 
     public void Respawn()
