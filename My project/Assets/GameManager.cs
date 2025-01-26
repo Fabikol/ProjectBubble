d9c60fc5a544d6f15b352b25175a642a7516b1a1
Fabian Reschke <fabianreschke123@gmail.com>
2025-01-26 13:26:24 +0100

    Added Gamemanager

---
+++ My project/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player1, player2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {   
+        player1.GetComponent<PlayerControl>().OnDeath += (sender, e) => PlayerDeath(e);
+        player2.GetComponent<PlayerControl>().OnDeath += (sender, e) => PlayerDeath(e);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void PlayerDeath(PlayerControl player)
+    {
+        Debug.Log($"{player.gameObject.name} starb");
+    }
+}
